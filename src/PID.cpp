f2a9461203f968c5d4fe0efe4b6eead9ddf65a6a
TatyanaPolunina <tatyana.polunina@gmail.com>
2019-08-04 22:56:28 +0300

    fix indentations, add description

--- src/PID.cpp
+++ src/PID.cpp
@@ -2,18 +2,18 @@
 #include <iostream>
 
 PID::PID()
-	: p_error(0)
-	, i_error(0)
-	, d_error(0)
+    : p_error(0)
+    , i_error(0)
+    , d_error(0)
 {}
 
 PID::~PID() {}
 
 void PID::Init(double Kp_, double Ki_, double Kd_, int num_iter_to_tune) {
-  
-	Kp = Kp_;
-	Ki = Ki_;
-	Kd = Kd_;
+
+    Kp = Kp_;
+    Ki = Ki_;
+    Kd = Kd_;
     this->num_iter_to_tune = num_iter_to_tune;
     common_error = 0;
 }
@@ -21,8 +21,8 @@ void PID::Init(double Kp_, double Ki_, double Kd_, int num_iter_to_tune) {
 void PID::UpdateError(double cte) {
     ++num_iter;
     i_error += cte;
-	d_error = cte - p_error;
-	p_error = cte;
+    d_error = cte - p_error;
+    p_error = cte;
     total_i += abs(cte);
     common_error += std::abs(p_error);
     //exit if don't need to tune params
@@ -35,11 +35,12 @@ void PID::UpdateError(double cte) {
     if (common_error > tollerance)
     {
         double diff_error = prev_error - common_error;
-        //use -derivatives
-        tuneParams(p_error, total_i, d_error,  diff_error * grad_delta);
+        //use -derivatives for backpropagation
+        TuneParams(p_error, total_i, d_error, diff_error * grad_delta);
         std::cout << "K_params " << Kp << " " << Ki << " " << Kd << std::endl;
         std::cout << "common_error " << common_error << std::endl;
         prev_error = common_error;
+        //clean up for the next iteration
         common_error = 0;
         num_iter = 0;
         total_i = 0;
@@ -47,10 +48,10 @@ void PID::UpdateError(double cte) {
 }
 
 double PID::TotalError() {
-  return -Kp * p_error - Ki * i_error - Kd * d_error;
+    return -Kp * p_error - Ki * i_error - Kd * d_error;
 }
 
-void PID::tuneParams(double change_p, double change_i, double change_d, double delta)
+void PID::TuneParams(double change_p, double change_i, double change_d, double delta)
 {
     Kp += Kp * change_p * delta;
     Ki += Ki * change_i * delta;
--- src/PID.h
+++ src/PID.h
@@ -3,58 +3,61 @@
 
 #include <vector>
 class PID {
- public:
-  /**
-   * Constructor
-   */
-  PID();
+public:
+    /**
+    * Constructor
+    */
+    PID();
 
-  /**
-   * Destructor.
-   */
-  virtual ~PID();
+    /**
+    * Destructor.
+    */
+    virtual ~PID();
 
-  /**
-   * Initialize PID.
-   * @param (Kp_, Ki_, Kd_) The initial PID coefficients
-   */
-  void Init(double Kp_, double Ki_, double Kd_, int num_iter_to_tune);
+    /**
+    * Initialize PID.
+    * @param (Kp_, Ki_, Kd_) The initial PID coefficients
+    */
+    void Init(double Kp_, double Ki_, double Kd_, int num_iter_to_tune);
 
-  /**
-   * Update the PID error variables given cross track error.
-   * @param cte The current cross track error
-   */
-  void UpdateError(double cte);
+    /**
+    * Update the PID error variables given cross track error.
+    * @param cte The current cross track error
+    */
+    void UpdateError(double cte);
 
-  /**
-   * Calculate the total PID error.
-   * @output The total PID error
-   */
-  double TotalError();
+    /**
+    * Calculate the total PID error.
+    * @output The total PID error
+    */
+    double TotalError();
 
- private:
-    void tuneParams(double change_p, double change_i, double change_d, double delta);
-  /**
-   * PID Errors
-   */
-  double p_error;
-  double i_error;
-  double d_error;
+private:
+    /**
+    tune the PID K parameters using backpropagation approach
+    */
+    void TuneParams(double change_p, double change_i, double change_d, double delta);
+    /**
+    * PID Errors
+    */
+    double p_error;
+    double i_error;
+    double d_error;
 
-  /**
-   * PID Coefficients
-   */ 
-  double Kp;
-  double Ki;
-  double Kd;
-  double tollerance = 0.001;
-  double grad_delta = 0.01;
-  int num_iter_to_tune;
-  int num_iter = 0;
-  double common_error = 0;
-  double prev_error = 0;
-  double total_i = 0;
-  double best_err = 1000;
+    /**
+    * PID Coefficients
+    */
+    double Kp;
+    double Ki;
+    double Kd;
+    double tollerance = 0.001;
+    double grad_delta = 0.01;
+    int num_iter_to_tune;
+    int num_iter = 0;
+    double common_error = 0;
+    double prev_error = 0;
+    double total_i = 0;
+    double best_err = 1000;
 };
 
 #endif  // PID_H
